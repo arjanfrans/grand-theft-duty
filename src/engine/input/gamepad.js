4efe103ac651c5a851b50bdd8f78c42d6c5921e6
Arjan Frans <arjanfrans.com@gmail.com>
2015-11-05 00:48:58 +0100

    add gamepad api support

---
+++ src/engine/input/gamepad.js
@@ -0,0 +1,82 @@
+let GamepadMicro = require('./gamepad-micro');
+
+let _gp = new GamepadMicro();
+
+let _gamepads = [];
+
+let Gamepad = {
+    isDown: function (index, button) {
+        if (_gamepads.length > 0) {
+            // FIXME rewrite gamepad-micro
+            let gamepad = _gamepads[index + 1];
+
+            if (gamepad.buttons[button] && gamepad.buttons[button].held) {
+                return true;
+            }
+
+            return false;
+        }
+    },
+    isStickDown: function (index, stick, direction) {
+        if (_gamepads.length > 0) {
+            // FIXME rewrite gamepad-micro
+            let gamepad = _gamepads[index + 1];
+
+            if (stick === 'right' && gamepad.rightStick) {
+                if (direction === 'right') {
+                    if (gamepad.rightStick.x > 0.5) {
+                        return true;
+                    }
+                } else if (direction === 'left') {
+                    if (gamepad.rightStick.x < -0.5) {
+                        return true;
+                    }
+                } else if (direction === 'up') {
+                    if (gamepad.rightStick.y < -0.5) {
+                        return true;
+                    }
+                } else if (direction === 'down') {
+                    if (gamepad.rightStick.y > 0.5) {
+                        return true;
+                    }
+                } else {
+                    return false;
+                }
+            }
+
+            if (stick === 'left' && gamepad.leftStick) {
+                if (direction === 'right') {
+                    if (gamepad.leftStick.x > 0.5) {
+                        return true;
+                    }
+                } else if (direction === 'left') {
+                    if (gamepad.leftStick.x < -0.5) {
+                        return true;
+                    }
+                } else if (direction === 'up') {
+                    if (gamepad.leftStick.y < -0.5) {
+                        return true;
+                    }
+                } else if (direction === 'down') {
+                    if (gamepad.leftStick.y > 0.5) {
+                        return true;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+};
+
+_gp.onUpdate(function (gamepads) {
+    _gamepads = gamepads;
+
+    if (_gp.gamepadsConnected) {
+
+    } else {
+
+    }
+});
+
+module.exports = Gamepad;
--- src/engine/input/player.js
+++ src/engine/input/player.js
@@ -1,8 +1,7 @@
-'use strict';
-
 let debug = require('debug')('game:engine/input/player');
 
 let Keyboard = require('./keyboard');
+let Gamepad = require('./gamepad');
 
 class PlayerInput {
     constructor (player) {
@@ -10,17 +9,17 @@ class PlayerInput {
     }
 
     update () {
-        if (Keyboard.isDown(Keyboard.UP)) {
+        if (Keyboard.isDown(Keyboard.UP) || Gamepad.isStickDown(0, 'left', 'up')) {
             this.player.move('up');
-        } else if (Keyboard.isDown(Keyboard.DOWN)) {
+        } else if (Keyboard.isDown(Keyboard.DOWN) || Gamepad.isStickDown(0, 'left', 'down')) {
             this.player.move('down');
         } else {
             this.player.stopMoving();
         }
 
-        if (Keyboard.isDown(Keyboard.RIGHT)) {
+        if (Keyboard.isDown(Keyboard.RIGHT) || Gamepad.isStickDown(0, 'right', 'right')) {
             this.player.turn('right');
-        } else if (Keyboard.isDown(Keyboard.LEFT)) {
+        } else if (Keyboard.isDown(Keyboard.LEFT) || Gamepad.isStickDown(0, 'right', 'left')) {
             this.player.turn('left');
         } else {
             this.player.stopTurning();

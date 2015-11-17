ff2f6110f03cd801062bdb44d100526133364bf4
Arjan Frans <arjanfrans.com@gmail.com>
2015-11-17 22:36:02 +0100

    add webpack deploy config

---
+++ webpack.production.config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let path = require('path');
+let webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.js$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+};

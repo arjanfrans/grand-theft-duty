e4d9767ea88c48ef3f90018f5ed806dc17119db5
Arjan Frans <arjanfrans.com@gmail.com>
2015-11-17 22:28:18 +0100

    publish: set menu as default screen
    generated from commit 8ad9f9b0dcf0b5325ac0dd43e638878581c31ea5

---
+++ assets/generate_spritesheets.sh
@@ -0,0 +1,4 @@
+node ../node_modules/spritesheet-js/index.js -p ./spritesheets -f jsonarray --name tiles --powerOfTwo true images/tiles/*.png
+node ../node_modules/spritesheet-js/index.js -p ./spritesheets -f jsonarray --name dude --powerOfTwo true images/character/*.png
+node ../node_modules/spritesheet-js/index.js -p ./spritesheets -f jsonarray --name world --powerOfTwo true images/world/*.png
+node ../node_modules/spritesheet-js/index.js -p ./spritesheets -f jsonarray --name ui --powerOfTwo true images/ui/*.png images/weapons/*.png
